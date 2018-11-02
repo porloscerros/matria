8c2c4d985b10c6ec7def39f6a30e4f7e1a2feeb2
Pablo E Pérez <porloscerros@gmail.com>
2018-11-02 17:46:20 -0300

    remove contacts from dashboard

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -15,10 +15,6 @@
     </div>
 
     <div class="col-xl-4 col-sm-6 mb-3">
-      @include('admin/dashboard/_contacts')
-    </div>
-
-    <div class="col-xl-4 col-sm-6 mb-3">
       @include('admin/dashboard/_users')
     </div>
   </div>
--- resources/views/admin/shared/sidebar.blade.php
+++ resources/views/admin/shared/sidebar.blade.php
@@ -20,13 +20,6 @@
         </a>
     </li>
 
-    <li class="nav-item" role="presentation" data-toggle="tooltip" data-placement="right" title="@lang('dashboard.contacts')">
-        <a class="nav-link {{ request()->route()->named('admin.contacts.*') ? 'active' : '' }}" href="{{ route('admin.contacts.index') }}">
-            <i class="fa fa-address-book" aria-hidden="true"></i>&nbsp;
-            <span class="nav-link-text">@lang('dashboard.contacts')</span>
-        </a>
-    </li>
-
     <li class="nav-item" role="presentation" data-toggle="tooltip" data-placement="right" title="@lang('dashboard.customize-site')">
         <a class="nav-link {{ request()->route()->named('admin.customize-site.*') ? 'active' : '' }}" href="{{ route('admin.customize-site.index') }}">
             <i class="fa fa-tachometer" aria-hidden="true"></i>&nbsp;
