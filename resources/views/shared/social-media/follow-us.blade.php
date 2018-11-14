39d701fb49b72c779d4281ec8329e661fcd2e3bb
Pablo E Pérez <porloscerros@gmail.com>
2018-11-14 02:35:25 -0300

    load facebook follow us plugin on document ready

--- resources/views/shared/social-media/follow-us.blade.php
+++ resources/views/shared/social-media/follow-us.blade.php
@@ -6,17 +6,17 @@
     @push('inline-scripts')
         <!-- Load Facebook SDK for JavaScript -->
         <div id="fb-root"></div>
-        <script>(function(d, s, id) {
-                var js, fjs = d.getElementsByTagName(s)[0];
-                if (d.getElementById(id)) return;
-                js = d.createElement(s); js.id = id;
-                js.src = 'https://connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v3.2';
-                fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));
+        <script>
+            $( document ).ready(function() {
+                (function(d, s, id) {
+                    var js, fjs = d.getElementsByTagName(s)[0];
+                    if (d.getElementById(id)) return;
+                    js = d.createElement(s); js.id = id;
+                    js.src = 'https://connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v3.2';
+                    fjs.parentNode.insertBefore(js, fjs);
+                }(document, 'script', 'facebook-jssdk'));
+            });
         </script>
-
-        <!-- Load Twitter Share JavaScript -->
-        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
     @endpush
 @endif
 @if($section->hasCustomProperty('twitter'))
@@ -24,4 +24,9 @@
     <a href="https://twitter.com/{{$section->getCustomProperty('twitter')}}?ref_src=twsrc%5Etfw" class="twitter-follow-button mx-1" data-size="large" data-show-screen-name="false" data-lang="es" data-show-count="false">
         Follow @ {{$section->getCustomProperty('twitter')}}
     </a>
+
+    @push('inline-scripts')
+        <!-- Load Twitter Share JavaScript -->
+        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    @endpush
 @endif
