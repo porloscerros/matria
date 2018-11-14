48e0e9650dc8987dfc67f129b49bbb5b6d9feeb5
Pablo E Pérez <porloscerros@gmail.com>
2018-11-14 19:08:05 -0300

    contact-us section responsive

--- resources/views/home-sections/contact-us.blade.php
+++ resources/views/home-sections/contact-us.blade.php
@@ -13,77 +13,81 @@
                 </h3>
             </div>
 
-            <div class="d-flex flex-md-column p-5 bg-translucent">
-                <div class="d-flex flex-md-row justify-content-around">
-                    <div class="justify-content-between">
-                        @if($section->hasCustomProperty('phone'))
-                            <div class="align-items-center h4">
-                                <span class="fa fa-whatsapp"></span>  @lang('contact-us.phone')/Wapp:
-                                <a href="tel:{{$section->getCustomProperty('phone')}}"> {{$section->getCustomProperty('phone')}}</a>
-                            </div>
-                        @endif
-                        @if($section->hasCustomProperty('email'))
-                            <div class="align-items-center h4">
-                                <span class="fa fa-envelope-o"></span> Email:
-                                <a href="mailto:{{$section->getCustomProperty('email')}}"> {{$section->getCustomProperty('email')}}</a>
-                            </div>
-                        @endif
-                        @if($section->hasCustomProperty('location'))
-                            <div class="align-items-center h4">
-                                <span class="fa fa-map-marker"></span> @lang('contact-us.location'):
-                                <a href=""  data-toggle="modal" data-target="#mapModal"> {{$section->getCustomProperty('location')}}</a>
-                            </div>
-                            <!-- Map Modal -->
-                            @include('shared.modals.map')
+            <div class="d-flex flex-column p-5 bg-translucent">
+                <div class="clearfix">
+                    <div class="row">
 
-                        @endif
-                        @if($section->hasSocialMedia())
-                            <hr>
+                        <div class="col-md-6" id="contact-data">
+                            @if($section->hasCustomProperty('phone'))
+                                <div class="align-items-center">
+                                    <span class="fa fa-whatsapp"></span>  @lang('contact-us.phone')/Wapp:
+                                    <a href="tel:{{$section->getCustomProperty('phone')}}"> {{$section->getCustomProperty('phone')}}</a>
+                                </div>
+                            @endif
+                            @if($section->hasCustomProperty('email'))
+                                <div class="align-items-center">
+                                    <span class="fa fa-envelope-o"></span> Email:
+                                    <a href="mailto:{{$section->getCustomProperty('email')}}"> {{$section->getCustomProperty('email')}}</a>
+                                </div>
+                            @endif
+                            @if($section->hasCustomProperty('location'))
+                                <div class="align-items-center">
+                                    <span class="fa fa-map-marker"></span> @lang('contact-us.location'):
+                                    <a href=""  data-toggle="modal" data-target="#mapModal"> {{$section->getCustomProperty('location')}}</a>
+                                </div>
+                                <!-- Map Modal -->
+                                @include('shared.modals.map')
 
-                            <!-- Social Media Elements -->
-                            <div class="">
-                                <p class="align-items-center h4">@lang('contact-us.social-media'):</p>
-                            </div>
-                            <div class="">
-                                @include('shared.social-media.links')
-                            </div>
-                            <!-- Social Media Elements -->
-                        @endif
-                    </div>
-
-                    <div class="justify-content-between">
-                        @include('shared/alerts')
-                        {!! Form::open(['route' => 'mail-to-admin']) !!}
-
-                        <div class="form-row">
-                            <div class="form-group col-md-6">
-                                {!! Form::text('name', null, ['class' => 'form-control' . ($errors->has('name') ? ' is-invalid' : ''), 'placeholder' => __('contact-us.name'), 'required']) !!}
-
-                                @if ($errors->has('name'))
-                                    <span class="invalid-feedback">{{ $errors->first('name') }}</span>
-                                @endif
-                            </div>
-
-                            <div class="form-group col-md-6">
-                                {!! Form::text('email', null, ['class' => 'form-control' . ($errors->has('email') ? ' is-invalid' : ''), 'placeholder' => __('contact-us.email'), 'required']) !!}
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback">{{ $errors->first('email') }}</span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            {!! Form::textarea('msg', null, ['class' => 'form-control' . ($errors->has('msg') ? ' is-invalid' : ''), 'placeholder' => __('contact-us.message'), 'rows' => 4, 'required' => 'required']) !!}
-
-                            @if ($errors->has('msg'))
-                                <span class="invalid-feedback">{{ $errors->first('msg') }}</span>
+                            @endif
+                            @if($section->hasSocialMedia())
+                                <hr>
+                                <!-- Social Media Elements -->
+                                <div class="">
+                                    <p class="align-items-center">@lang('contact-us.social-media'):</p>
+                                </div>
+                                <div class="text-center">
+                                    @include('shared.social-media.links')
+                                </div>
+                                <!-- Social Media Elements -->
+                                <hr>
                             @endif
                         </div>
 
-                        {!! Form::submit(__('forms.actions.send'), ['class' => 'btn btn-action pull-right']) !!}
+                        <div class="col-md-6">
+                            @include('shared/alerts')
+                            {!! Form::open(['route' => 'mail-to-admin']) !!}
 
-                        {!! Form::close() !!}
+                            <div class="form-row">
+                                <div class="form-group col-md-6">
+                                    {!! Form::text('name', null, ['class' => 'form-control' . ($errors->has('name') ? ' is-invalid' : ''), 'placeholder' => __('contact-us.name'), 'required']) !!}
+
+                                    @if ($errors->has('name'))
+                                        <span class="invalid-feedback">{{ $errors->first('name') }}</span>
+                                    @endif
+                                </div>
+
+                                <div class="form-group col-md-6">
+                                    {!! Form::text('email', null, ['class' => 'form-control' . ($errors->has('email') ? ' is-invalid' : ''), 'placeholder' => __('contact-us.email'), 'required']) !!}
+
+                                    @if ($errors->has('email'))
+                                        <span class="invalid-feedback">{{ $errors->first('email') }}</span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                {!! Form::textarea('msg', null, ['class' => 'form-control' . ($errors->has('msg') ? ' is-invalid' : ''), 'placeholder' => __('contact-us.message'), 'rows' => 4, 'required' => 'required']) !!}
+
+                                @if ($errors->has('msg'))
+                                    <span class="invalid-feedback">{{ $errors->first('msg') }}</span>
+                                @endif
+                            </div>
+
+                            {!! Form::submit(__('forms.actions.send'), ['class' => 'btn btn-action pull-right']) !!}
+
+                            {!! Form::close() !!}
+                        </div>
+
                     </div>
                 </div>
             </div>
--- resources/views/shared/social-media/links.blade.php
+++ resources/views/shared/social-media/links.blade.php
@@ -1,24 +1,24 @@
 @if($section->hasCustomProperty('fb_page'))
     <!-- Facebook -->
     <a href="https://www.facebook.com/{{$section->getCustomProperty('fb_page')}}/" class="fb-ic" target="_blank">
-        <i class="fa fa-facebook fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+        <i class="fa fa-facebook fa-lg white-text mr-md-5 mr-3"> </i>
     </a>
 @endif
 @if($section->hasCustomProperty('instagram'))
     <!--Instagram-->
     <a href="https://www.instagram.com/{{$section->getCustomProperty('instagram')}}/" class="ins-ic" target="_blank">
-        <i class="fa fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+        <i class="fa fa-instagram fa-lg white-text mr-md-5 mr-3"> </i>
     </a>
 @endif
 @if($section->hasCustomProperty('twitter'))
     <!-- Twitter -->
     <a href="https://twitter.com/{{$section->getCustomProperty('twitter')}}" class="tw-ic" target="_blank">
-        <i class="fa fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+        <i class="fa fa-twitter fa-lg white-text mr-md-5 mr-3"> </i>
     </a>
 @endif
 @if($section->hasCustomProperty('google_plus'))
     <!-- Twitter -->
     <a href="https://plus.google.com/{{$section->getCustomProperty('google_plus')}}" class="tw-ic" target="_blank">
-        <i class="fa fa-google-plus fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+        <i class="fa fa-google-plus fa-lg white-text mr-md-5 mr-3"> </i>
     </a>
 @endif
