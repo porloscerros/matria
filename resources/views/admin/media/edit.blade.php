33c0c782703bcb21975a5c104774ee9419910dba
Pablo E Pérez <porloscerros@gmail.com>
2018-11-15 01:37:01 -0300

    fix media preview on edit form

--- resources/views/admin/media/edit.blade.php
+++ resources/views/admin/media/edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="d-flex justify-content-center">
-        <div class="col-md-6"
+        <div class="col-md-6">
             <div class="card">
                 <img src="{{ $medium->getUrl('thumb') }}" alt="{{ $medium->name }}" class="card-img-top">
             </div>
