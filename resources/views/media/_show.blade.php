bb417687198ce6693b510a71a414b9ad1f789f5c
Pablo E Pérez <porloscerros@gmail.com>
2018-11-24 18:37:23 -0300

    font size for post show, post list cards, media list cards, btn-action

--- resources/views/media/_show.blade.php
+++ resources/views/media/_show.blade.php
@@ -3,17 +3,15 @@
         <img src="{{ $medium->getUrl('thumb') }}" alt="{{ $medium->name }}" class="card-img-top">
     </a>
 
-    <div class="card-body text-dark">
+    <div class="card-body text-dark text-muted">
         <p class="card-text text-center">
-            <small class="text-muted">
-                <i class="fa fa-tag" aria-hidden="true"></i>
-                @foreach($medium->tags as $tag)
-                    <a href="{{ route('media', ['q' => $tag->name]) }}">
-                        #{{ $tag->name }}
-                    </a>
-                @endforeach
-                {!! $medium->getCustomProperty('description') !!}
-            </small>
+            <i class="fa fa-tag" aria-hidden="true"></i>
+            @foreach($medium->tags as $tag)
+                <a href="{{ route('media', ['q' => $tag->name]) }}">
+                    #{{ $tag->name }}
+                </a>
+            @endforeach
+            {!! $medium->getCustomProperty('description') !!}
         </p>
     </div>
 </div>
