6556d47449d5023aee07fe049f24738d8d5952c0
Pablo E Pérez <porloscerros@gmail.com>
2018-11-26 17:06:33 -0300

    porloscerros footer with custom font

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -1,7 +1,7 @@
 <!-- Footer -->
 <footer class="font-small bg-white text-dark m-0">
     <!-- Copyright -->
-    <div class="footer-copyright text-center py-3">© 2018 Copyright:
+    <div class="footer-copyright text-center py-3 h4">© 2018 Copyright:
         <a href="/"> {{ config('app.name') }}</a>
     </div>
     <!-- Copyright -->
--- resources/views/shared/porloscerros-footer.blade.php
+++ resources/views/shared/porloscerros-footer.blade.php
@@ -1,5 +1,5 @@
-<footer class="footer bg-dark text-light m-0 text-right">
+<footer class="footer bg-dark text-light m-0 text-right porloscerros">
     <div class="container">
-        <p class="pr-3">@lang('sections.footer.made-with-love') <i class="fa fa-heart text-danger" aria-hidden="true"></i>@lang('sections.footer.by') <a href="https://porloscerros.github.io" target="_blank" class="">porloscerros </a></p>
+        <p class="pr-3">@lang('sections.footer.made-with-love') <i class="fa fa-heart text-danger" aria-hidden="true"></i>@lang('sections.footer.by') <a href="https://porloscerros.github.io" target="_blank">porloscerros </a></p>
     </div>
 </footer>
