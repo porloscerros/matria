7d410b6e254ac608ca2999c594f9d799145600be
Pablo E Pérez <porloscerros@gmail.com>
2018-11-26 02:21:35 -0300

    minor changes in home views titles

--- resources/views/home-sections/contact-us.blade.php
+++ resources/views/home-sections/contact-us.blade.php
@@ -4,7 +4,7 @@
         <div class="d-flex justify-content-center flex-column h-100">
 
             <div class="d-flex align-items-start flex-column">
-                <h3 class="align-self-center label1">
+                <h3 class="align-self-center label1 pb-3">
                     @if( $section->hasCustomProperty('title') )
                         {{ $section->getCustomProperty('title') }}
                     @else
--- resources/views/home-sections/posts.blade.php
+++ resources/views/home-sections/posts.blade.php
@@ -3,7 +3,7 @@
     <div class="container h-100 bg-translucent">
         <div class="d-flex justify-content-center flex-column h-100">
             <div class="d-flex align-items-start flex-column">
-                <h3 class="align-self-center label1">
+                <h3 class="align-self-center label1 pb-3">
                 @if( $section->hasCustomProperty('title') )
                     {{ $section->getCustomProperty('title') }}
                 @else
