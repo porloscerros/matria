ec32a7a56c2cdd085d74fad11b8736b8965e317b
Pablo E Pérez <porloscerros@gmail.com>
2018-11-27 13:50:58 -0300

    home page social media description

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.home')
 
 @section('social-image','https://matriatallados.com.ar/storage/146/portada.png')
+@section('social-description', 'Sitio Web para mostrar mis trabajos.')
 
 @section('content')
     @foreach($sections as $section)
